ba975816c79fc443ded484976dc3667e13fc66ad
takatakatakashi <iverson3kobe0824@gmil.com>
2020-02-06 14:17:46 +0900

    add

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '<用意したElastic IP>', user: 'ec2-user', roles: %w{app db web}
+server '18.176.253.195', user: 'ec2-user', roles: %w{app db web}
